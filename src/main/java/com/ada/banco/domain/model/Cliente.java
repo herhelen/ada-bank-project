d3eb3a1bee99c6f34965bde535a9f4384ced42af
Helen Her <her.helen@gmail.com>
2023-11-18 23:14:39 -0300

    add Cliente model

---
+++ src/main/java/com/ada/banco/domain/model/Cliente.java
@@ -0,0 +1,43 @@
+package com.ada.banco.domain.model;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+}
