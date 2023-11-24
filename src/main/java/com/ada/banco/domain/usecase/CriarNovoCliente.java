7e5e3b19f086e6a3b97c072bc57e604c7a121e19
Helen Her <her.helen@gmail.com>
2023-11-24 13:55:53 -0300

    create use case to create new client

---
+++ src/main/java/com/ada/banco/domain/usecase/CriarNovoCliente.java
@@ -0,0 +1,10 @@
+package com.ada.banco.domain.usecase;
+
+import com.ada.banco.domain.model.Cliente;
+
+public class CriarNovoCliente {
+
+    public void execute(Cliente cliente) {
+
+    }
+}
---
+++ src/test/java/com/ada/banco/domain/usecase/CriarNovoClienteTest.java
@@ -0,0 +1,18 @@
+package com.ada.banco.domain.usecase;
+
+import com.ada.banco.domain.model.Cliente;
+import org.junit.jupiter.api.Test;
+
+public class CriarNovoClienteTest {
+
+
+    @Test
+    public void deveCriarUmNovoCliente() {
+        // Given
+        Cliente titular = new Cliente("Pedro", "12345612366");
+
+        // nao esta cadastrado
+
+    }
+
+}
